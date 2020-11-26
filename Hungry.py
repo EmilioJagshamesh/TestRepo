bcfb517cfe840e61ad25f72924b7e785cc081743
EmilioJagshamesh <elmijito1990@gmail.com>
2020-11-26 15:37:04 -0600

    tuple for py and txt file

--- Hungry.py
+++ Hungry.py
@@ -1,5 +1,11 @@
-hungry = input("Are u hungry?")
+hungry = "yes"
 if hungry == "yes":
     print("so eat")
 else:
     print("no food for u u fuk")
+
+print("working with a tuple")
+
+vals = (1,2,3)
+print(vals)
+print(vals[1])
